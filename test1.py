76283566154bffd902a636e711801e791d7f7ec2
TheMeteors <meteors12845@gmail.com>
2025-03-04 20:00:33 -0500

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,51 @@
+def myfunction( name):
+  print(name,"is a function")
+
+print("stink machine")
+#print("stink level 1")
+#print("phhhh!")
+i = 1
+o = 2
+while i < 6:
+    i += 1
+    if i == 1:
+        continue
+    print("stink level :",i)
+    print("phhhh!")
+    print("how bad was that")
+    if i >= 3 and i <= 4:
+        continue
+    else:
+        print("bad")   
+    print("ok")
+    if i > 4:
+        break
+    
+fruit = ["banana apple"]
+for x in range(2,30,3):
+    print(x)
+#rgjjdhcvdfbshjsvcdhv
+
+x=str("how bad was that")
+p=str("stink level 2")
+print(x)
+
+myfunction("Mason")
+#while o<9:
+#    o +=1
+#    print("x-1",o)
+#if o == 9:
+#    o+=1
+#    print("x somewhere")
+#while o>9:
+#    o +=1
+#    print("x+1",o)
+#    if o== 12: break
+while o<12:
+    o += 1
+    if o ==10:
+        print ("x somewhere")
+    if o<9:
+        print("x-1",o)
+    else:
+        print("x+1",o)
